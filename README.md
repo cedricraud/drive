c969b8ed4df628c19d030ddba36ddd02e52a7b45
Cédric Raud <cedric.raud@gmail.com>
2013-02-05 11:25:29 +0100

    Initial release
    - Street View previews on the map
    - Directions panel
    - Pretty link to gif animations

---
+++ README.md
@@ -0,0 +1,21 @@
+# Drive
+
+Drive is an [node](http://nodejs.org) web app to explore the world from the couch.
+Choose an start address, a destination and press Drive: the app will fetch the Google Street View panoramas for you and generate an animated trip.
+
+## How to install
+
+Required binaries:
+
+* [Gifsicle](http://www.lcdf.org/gifsicle/)
+* [ImageMagick](http://www.imagemagick.org/script/index.php)
+
+Run npm install after deployment.
+
+## Changelog
+
+0.1.0
+
+* Street View previews on the map
+* Directions panel
+* Pretty link to gif animations
---
+++ app.js
@@ -0,0 +1,142 @@
+var http = require('http'),
+	express = require('express'),
+	fs = require('fs'),
+	crypto = require('crypto'),
+	exec = require('child_process').exec,
+	gm = require('gm'),
+	dateformat = require('dateformat');
+
+
+function Drive () {
+	"use strict";
+	var started = false;
+	var optimised = true; // convert to gif before saving (stats: 268jpg: optimised 13s, not optimised 31s)
+	var app;
+
+	var init = function() {
+		// Setup App
+		app = express();
+		app.use(express.urlencoded());
+		app.use('/assets', express.static(__dirname + '/assets'));
+		app.use(express.limit('2mb'));
+
+		// Routes
+		app.get('/', function(req, res){
+			res.sendfile(__dirname + '/index.html');
+		});
+		app.all('/trip/:trip', function(req, res, next) {
+			var id = getId(req.params.trip);
+			log(id, 'Query trip: ' + req.params.trip.replace(/\./g, ' '));
+			if (fs.existsSync(__dirname + '/exports/' + id + '.gif'))
+				sendGif(req, res, id);
+			else if (req.body.records)
+				downThemAll(req, res, id, JSON.parse(req.body.records));
+			else
+				res.redirect(301, '/');
+		});
+	};
+
+	var downThemAll = function(req, res, id, files) {
+		var count = 0;
+		var total = files.length;
+		var remaining = total;
+		log(id, 'Downloading ' + total + ' streetviews');
+		function leechThemAll() {
+			if (files.length > 0) {
+				var index = ++count;
+				var path = files.shift();
+				var file = path.substring(path.lastIndexOf('/') + 1);
+				http.get(path, function(fileres) {
+					var filename = id + '-' + zeroPad(index, 1000) + '.jpg';
+					var p = __dirname + '/temp/';
+					fileres.on("end", function() { --remaining; leechThemAll() });
+
+					if (optimised) { // Use a GraphicsMagick stream
+						gm(fileres, filename).write(p + filename.replace('jpg', 'gif'), function (err) {
+							if (err) console.log(err);
+						});
+					}
+					else {
+						var stream = fs.createWriteStream(p + filename);
+						fileres.pipe(stream);
+					}
+				}).on('error', function(e) {
+					log(id, 'Error: ' + e.message);
+					leechThemAll();
+				});
+			}
+			else if (remaining === 0)
+				gify(req, res, id);
+		}
+		for (var i = 0; i < 5; i++)
+			leechThemAll();
+	};
+
+	var gify = function(req, res, id) {
+		var filename = id + '.gif';
+		log(id, 'Generating gif');
+		exec('cd ' + __dirname + '/temp;' +
+			(optimised ? '' : 'mogrify -format gif ' + id + '*.jpg;') +
+			'gifsicle --delay=17 --method blend-diversity --colors 256 --loop ' + id + '*.gif > ../exports/' + filename +
+			';rm ' + id + '*',
+			function(error, stdout, stderr) {
+				sendGif(req, res, id);
+		});
+	}
+
+	var sendGif = function(req, res, id) {
+		var readStream = fs.createReadStream(__dirname + '/exports/' + id + '.gif');
+		res.writeHead(200, { 'Content-Type': 'image/gif' });
+		readStream.pipe(res);
+		log(id, 'Sending gif');
+	}
+
+	var log = function(id, message) {
+		var text = '[' + dateformat("hh:MM:ss") + ']' + (id ? '[' + id + '] ' : ' ') + message;
+		console.log(text);
+	};
+
+	var zeroPad = function(nr, base){
+		base = base || 10;
+		var  len = (String(base).length - String(nr).length)+1;
+		return len > 0 ? new Array(len).join('0')+nr : nr;
+	};
+
+	var getId = function(query) {
+		return crypto.createHash('md5').update(query).digest("hex").substring(0, 5);
+	};
+
+	var start = function(callback) {
+		var port = 80;
+		app.listen(port, '0.0.0.0', 551, callback).on('error', function (e) {
+			if(e.code == 'EADDRINUSE') log(null, "Error, address is already in use.");
+			started = false;
+		});
+		started = true;
+		log(null, 'Starting server on port ' + port);
+	};
+
+	var stop = function() {
+		app.close();
+		started = false;
+		log(null, 'Stopping server');
+	};
+
+	var isStarted = function() {
+		return started;
+	};
+
+	init();
+	return {
+		'isStarted': isStarted,
+		'start': start,
+		'stop': stop,
+		'app': app
+	};
+}
+
+var drive = new Drive();
+if (module.parent)
+	exports.app = drive.app;
+else
+	drive.start();
